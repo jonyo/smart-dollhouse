a07adc0f16f0fdd88fe3a56d09e913df590353fb
Jonathan Foote <jf@jonathanfoote.com>
2021-09-16 10:48:56 -0500

    Refactor touch event handling

--- touch.py
+++ touch.py
@@ -1,92 +1,115 @@
 import pygame
 import evdev
+from evdev import ecodes
 import os
-from collections import namedtuple
 
-# surface resolution
-surfaceSize = (320, 240)
+class TouchAxis(object):
+    def __init__(self, min: int, max: int, invert: bool) -> None:
+        if min > max:
+            # is all good, just fix it...
+            print("Normally min is less than max, auto correcting by swapping values...")
+            realMin = max
+            max = min
+            min = realMin
+            del realMin
+            invert = not invert
+        # keep track of original values for debugging
+        self.min = min
+        self.max = max
+        self.invert = invert
+        # we do as much manipulation during init so those parts are only done once at the start
+        self.offset = min
+        self.delta = max - min
+        # prevent config issues leading to divide by 0 errors
+        assert self.delta is not 0
+        self.invert = invert
 
-# Used to map touch event from the screen hardware to the pygame surface pixels.
-# Works with 320 x 240, would need to adjust to callibrate for other screen sizes
-tftOrig = (3750, 180)
-tftEnd = (150, 3750)
-tftDelta = (tftEnd [0] - tftOrig [0], tftEnd [1] - tftOrig [1])
-tftAbsDelta = (abs(tftDelta[0]), abs(tftDelta[1]))
+    def scale(self, value) -> float:
+        """ Scales the value to be 0 to 1 based on original min/max """
+        value -= self.offset
+        # if out of bounds, set to min or max
+        if value > self.delta:
+            return 1.0
+        if value <= 0:
+            return 0.0
+        if self.invert:
+            value = self.delta - value
+        return float(value) / float(self.delta)
 
-# We use evdev to read events from our touchscreen
-# (The device must exist and be properly installed for this to work)
+class TouchHandler(object):
+    # surface resolution
+    surfaceSize = (320, 240)
+    # touch axis details, for 320x240 2.8" TFT touchscreen, would need adjusting for different sizes
+    # Based on axis as reported by evdev (which is swapped.. we correct for by setting swapAxis further down)
+    xTouch = TouchAxis(min=180, max=3750, invert=False)
+    yTouch = TouchAxis(min=150, max=3750, invert=True)
+
+    # latest x/y values reported by touch events
+    x_val = None
+    y_val = None
+
+    # going from evdev x/y values to touchscreen, is x and y swapped?  Normally yes but maybe you want different
+    # orientation, if so set to False
+    swapAxis = True
+
+    # simplify event handling, no need to use categorize() just to have fancy way to tell if button is down or up...
+    KEY_UP = 0
+    KEY_DOWN = 1
+
+    def _screenCoords(self):
+        """ calculate the surface screen coordinates of the last x/y reported values from evdev """
+        assert self.x_val is not None and self.y_val is not None
+        if self.swapAxis:
+            x = self.yTouch.scale(self.y_val)
+            y = self.xTouch.scale(self.x_val)
+        else:
+            x = self.xTouch.scale(self.x_val)
+            y = self.yTouch.scale(self.y_val)
+        return (
+            int(x * self.surfaceSize[0]),
+            int(y * self.surfaceSize[1])
+        )
+
+    def handleEvent(self, event: evdev.events.InputEvent or None) -> None:
+        if event == None:
+            return
+
+        if event.type == ecodes.EV_ABS:
+            if event.code == ecodes.ABS_X:
+                self.x_val = event.value
+            elif event.code == ecodes.ABS_Y:
+                self.y_val = event.value
+
+            if self.x_val is not None and self.y_val is not None:
+                (posx, posy) = self._screenCoords()
+                print("abs: x: %s y: %s - screen pixel x: %s y: %s"%( self.x_val, self.y_val, posx, posy ))
+                pygame.mouse.set_pos(posx, posy)
+                self.x_val = None
+                self.y_val = None
+
+        elif event.type == ecodes.EV_KEY:
+            if event.code == ecodes.BTN_TOUCH:
+                # this is actually the only key event reported by touchscreen, still it is good to check...
+                if event.value == self.KEY_DOWN:
+                    pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONDOWN))
+                elif event.value == self.KEY_UP:
+                    pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONUP))
+
 touch = evdev.InputDevice('/dev/input/touchscreen')
-
-# We make sure the events from the touchscreen will be handled only by this program
-# (so the mouse pointer won't move on X when we touch the TFT screen)
 touch.grab()
-
-def printDebug():
-    """ Print some info about the touch input device as evdev sees it """
-    print(touch)
-
-def printDebugMore():
-    """ Print even more info about the touch input device as evdev sees it """
-    print(touch.capabilities())
-
-def getPixelsFromCoordinates(coords):
-    """ Convert evdev "hardware" touch coordinates to pygame surface pixel coordinates """
-    if tftDelta[0] < 0:
-        x = float(tftAbsDelta [0] - coords [0] + tftEnd [0]) / float(tftAbsDelta [0]) * float(surfaceSize [0])
-    else:
-        x = float(coords [0] - tftOrig [0]) / float(tftAbsDelta [0]) * float(surfaceSize [0])
-    if tftDelta [1] < 0:
-        y = float(tftAbsDelta [1] - coords [1] + tftEnd [1]) / float(tftAbsDelta [1]) * float(surfaceSize [1])
-    else:
-        y = float(coords [1] - tftOrig [1]) / float(tftAbsDelta [1]) * float(surfaceSize [1])
-    return (int(x), int(y))
-
-# Was useful to see what pieces I would need from the evdev events
-def printEvent(event):
-    print(evdev.categorize(event))
-    print("Value: {0}".format(event.value))
-    print("Type: {0}".format(event.type))
-    print("Code: {0}".format(event.code))
-
-Event = namedtuple('Event', ('down', 'pos'))
-
-class EventHandler(object):
-    def tick(self):
-        """ Get the relevent touch events and add to pygame, call before attempting to process pygame events """
-        # can't use select, we need it to be non-blocking...
-        # x and y updated in seperate events... keep track of one till the other comes in
-        X = None
-        Y = None
-        while True:
-            event = touch.read_one()
-            if event == None:
-                break
-
-            if event.type == evdev.ecodes.EV_ABS:
-                if event.code == 1:
-                    X = event.value
-                elif event.code == 0:
-                    Y = event.value
-                if X is not None and Y is not None:
-                    (posx, posy) = getPixelsFromCoordinates((X, Y))
-                    pygame.mouse.set_pos(posx, posy)
-                    X = None
-                    Y = None
-            elif event.type == evdev.ecodes.EV_KEY:
-                if event.code == 330:
-                    printEvent(event)
-                    if event.value == 1:
-                        # mouse down
-                        pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONDOWN))
-                    else:
-                        pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONUP))
-
-_touch = EventHandler()
+handler = TouchHandler()
 
 def beforePygameInit():
     """ Call before pygame.init() is called to initialize needed env vars specific to touch events """
     # stop reading events by pygame engine to avoid erratic mouse pointer behaviour
     os.putenv('SDL_MOUSEDEV', '/dev/null')
 
-def tick():
-    _touch.tick()
+def tick() -> None:
+    """ Get the relevent touch events and add to pygame, call before attempting to process pygame events """
+    while True:
+        # can't use select, we need it to be non-blocking...
+        event = touch.read_one()
+        if event == None:
+            # no more events this tick, break out of our little infinite loop we had going
+            break
+        handler.handleEvent(event)
--- touchtest.py
+++ touchtest.py
@@ -1,8 +1,7 @@
 import pygame
 import time
 import os
-import evdev
-import select
+import touch
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
@@ -10,69 +9,41 @@ os.putenv('SDL_AUDIODRIVER', 'dummy')
 
 surfaceSize = (320, 240)
 
+touch.beforePygameInit()
 pygame.init()
 
 lcd = pygame.display.set_mode(surfaceSize)
 pygame.mouse.set_visible(False)
 lcd.fill((0,0,0))
 pygame.display.update()
-# Used to map touch event from the screen hardware to the pygame surface pixels.
-# (Those values have been found empirically, but I'm working on a simple interactive calibration tool
-tftOrig = (3750, 180)
-tftEnd = (150, 3750)
-tftDelta = (tftEnd [0] - tftOrig [0], tftEnd [1] - tftOrig [1])
-tftAbsDelta = (abs(tftEnd [0] - tftOrig [0]), abs(tftEnd [1] - tftOrig [1]))
 
-def refresh():
-    pygame.display.update()
-    time.sleep(0.1)
+# block all events
+pygame.event.set_blocked(None)
 
-# We use evdev to read events from our touchscreen
-# (The device must exist and be properly installed for this to work)
-touch = evdev.InputDevice('/dev/input/touchscreen')
+# un-block events we care about
+pygame.event.set_allowed(pygame.QUIT)
+pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)
+pygame.event.set_allowed(pygame.MOUSEBUTTONUP)
+pygame.event.set_allowed(pygame.MOUSEMOTION)
 
-# We make sure the events from the touchscreen will be handled only by this program
-# (so the mouse pointer won't move on X when we touch the TFT screen)
-touch.grab()
-# Prints some info on how evdev sees our input device
-print(touch)
-# Even more info for curious people
-#print(touch.capabilities())
-
-# Here we convert the evdev "hardware" touch coordinates into pygame surface pixel coordinates
-def getPixelsFromCoordinates(coords):
-    # TODO check divide by 0!
-    if tftDelta [0] < 0:
-        x = float(tftAbsDelta [0] - coords [0] + tftEnd [0]) / float(tftAbsDelta [0]) * float(surfaceSize [0])
-    else:
-        x = float(coords [0] - tftOrig [0]) / float(tftAbsDelta [0]) * float(surfaceSize [0])
-    if tftDelta [1] < 0:
-        y = float(tftAbsDelta [1] - coords [1] + tftEnd [1]) / float(tftAbsDelta [1]) * float(surfaceSize [1])
-    else:
-        y = float(coords [1] - tftOrig [1]) / float(tftAbsDelta [1]) * float(surfaceSize [1])
-    return (int(x), int(y))
-
-# Was useful to see what pieces I would need from the evdev events
-def printEvent(event):
-    print(evdev.categorize(event))
-    print("Value: {0}".format(event.value))
-    print("Type: {0}".format(event.type))
-    print("Code: {0}".format(event.code))
+running = True
 
 # This loop allows us to write red dots on the screen where we touch it
-while True:
-    # TODO get the right ecodes instead of int
-    r,w,x = select.select([touch], [], [])
-    for event in touch.read():
-        if event.type == evdev.ecodes.EV_ABS:
-            if event.code == 1:
-                X = event.value
-            elif event.code == 0:
-                Y = event.value
-        elif event.type == evdev.ecodes.EV_KEY:
-            if event.code == 330 and event.value == 1:
-                printEvent(event)
-                p = getPixelsFromCoordinates((X, Y))
-                print("TFT: {0}:{1} | Pixels: {2}:{3}".format(X, Y, p [0], p [1]))
-                pygame.draw.circle(lcd, (255, 0, 0), p , 2, 2)
-                refresh()
+while running:
+    # process touch events
+    touch.tick()
+    # go through pygame event queue
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            print('mouse up')
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('mouse down')
+            x, y = pygame.mouse.get_pos()
+            print('position found: x %s y %s'%(x,y))
+            pygame.draw.circle(lcd, (255, 0, 0), (x, y) , 2, 2)
+            pygame.display.update()
+    time.sleep(0.1)
